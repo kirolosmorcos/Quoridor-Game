a59ee1f5e2a3ebdba9d916c4f0c892d1e67b037e
germinechawki <155088106+germinechawki@users.noreply.github.com>
2025-12-15 19:59:17 +0200

    Update Alph Beta Minimax.h

--- src/AI/Alph Beta Minimax.h
+++ src/AI/Alph Beta Minimax.h
@@ -2,10 +2,10 @@
 // Created by kiro3 on 12/10/2025.
 //
 
-#ifndef QUORIDOR_ALPH_BETA_MINIMAX_H
-#define QUORIDOR_ALPH_BETA_MINIMAX_H
+#ifndef QUORIDOR_ALPHA_BETA_MINIMAX_H
+#define QUORIDOR_ALPHA_BETA_MINIMAX_H
 
 #include "../Models/GameState.h"
 
-int alphaBeta(GameState gameState, int depth, int alpha, int beta, bool maximizingPlayer);
-#endif //QUORIDOR_ALPH_BETA_MINIMAX_H
+int AlphaBetaMinimax(GameState gameState, int depth, int alpha, int beta, bool maximizingPlayer);
+#endif //QUORIDOR_ALPHA_BETA_MINIMAX_H
