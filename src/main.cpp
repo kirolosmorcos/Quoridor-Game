f4de092f486792d477067ce8709155913c2e9555
Youssef-Ehab30 <167027905+Youssef-Ehab30@users.noreply.github.com>
2025-12-15 18:19:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,7 @@
-#include <iostream>
-#include <vector>
-#include "GameState.h"
-#include "Legal Moves.h"
-
-using namespace std;
+//
+// Created by kiro3 on 12/10/2025.
+//
 
 int main() {
-	GameState gameState;
-
-	gameState.turn = true;
-
-	gameState.player0_pos = 0 * 9 + 4; // (row 0, col 4)
-	gameState.player1_pos = 8 * 9 + 4; // (row 8, col 4)
-
-	// Initialize all horizontal walls to false
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 9; j++) {
-			gameState.horizontal_walls[i][j] = false;
-		}
-	}
-
-	// Initialize all vertical walls to false
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 8; j++) {
-			gameState.vertical_walls[i][j] = false;
-		}
-	}
-
-	// Initial wall counts
-	gameState.p0_walls = 10;
-	gameState.p1_walls = 10;
-
-
-
-
-	gameState.horizontal_walls[0][0] = true;
-	gameState.horizontal_walls[0][1] = true;
-	gameState.horizontal_walls[0][4] = true;
-	gameState.horizontal_walls[0][5] = true;
-
-
-	vector<GameState> legalWallMoves = horizontalWallMoves(gameState, false);
-	for (int i = 0; i < legalWallMoves.size(); i++) {
-		GameState newGameState = legalWallMoves[i];
-		for(int j = 0; j < 8; j++) {
-			for(int k = 0; k < 9; k++) {
-				cout << newGameState.horizontal_walls[j][k] << " ";
-			}
-			cout << endl;
-		}
-		cout << "////////////////\n";
-	}
-
-
-	return 0;
+    return 0;
 }
