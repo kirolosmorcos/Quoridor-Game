1c81a423cc34e26f69b8b8c6d01ed18049effe17
Joe <joe.ehab30@gmail.com>
2025-12-15 20:38:44 +0200

    Implement normal pawn moves and additional functions for legal moves

--- src/Game Logic/Legal Moves.cpp
+++ src/Game Logic/Legal Moves.cpp
@@ -1,23 +1,95 @@
-#include "../Models/GameState.h"
-#include <bits/stdc++.h>
+#include "Legal Moves.h"
 
-#include "BFS.h"
+vector<GameState> normalPawnMoves(GameState gameState, bool player) {
+	vector<GameState> legalNormalPawnMoves = vector<GameState>();
 
-using namespace std;
+	int currentPawnRow, currentPawnCol;
+	int opponentPawnRow, opponentPawnCol;
+	if (player) {
+		currentPawnRow = gameState.getrow(gameState.player1_pos);
+		currentPawnCol = gameState.getcol(gameState.player1_pos);
+		opponentPawnRow = gameState.getrow(gameState.player0_pos);
+		opponentPawnCol = gameState.getcol(gameState.player0_pos);
+	}
+	else {
+		currentPawnRow = gameState.getrow(gameState.player0_pos);
+		currentPawnCol = gameState.getcol(gameState.player0_pos);
+		opponentPawnRow = gameState.getrow(gameState.player1_pos);
+		opponentPawnCol = gameState.getcol(gameState.player1_pos);
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	// check moving up
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (currentPawnRow != 0) { // inside board
+		if (!gameState.horizontal_walls[currentPawnRow - 1][currentPawnCol] && // No wall upwards
+			((currentPawnCol != opponentPawnCol) || ((currentPawnRow - 1) != opponentPawnRow)) // Opponent pawn isn't upwards
+		   ) {
+			int nextPawnPosition = (currentPawnRow - 1) * 9 + currentPawnCol;
+			if (player) {
+				gameState.player1_pos = nextPawnPosition;
+			}
+			else {
+				gameState.player0_pos = nextPawnPosition;
+			}
+			legalNormalPawnMoves.push_back(gameState);
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	// check moving down
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (currentPawnRow != 8) { // inside board
+		if (!gameState.horizontal_walls[currentPawnRow][currentPawnCol] && // No wall downwards
+			((currentPawnCol != opponentPawnCol) || ((currentPawnRow + 1) != opponentPawnRow)) // Opponent pawn isn't downwards
+		   ){
+			int nextPawnPosition = (currentPawnRow + 1) * 9 + currentPawnCol;
+			if (player) {
+				gameState.player1_pos = nextPawnPosition;
+			}
+			else {
+				gameState.player0_pos = nextPawnPosition;
+			}
+			legalNormalPawnMoves.push_back(gameState);
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	// check moving left
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (currentPawnCol != 0) { // inside board
+		if (!gameState.vertical_walls[currentPawnRow][currentPawnCol - 1] && // No wall leftwards
+			((currentPawnRow != opponentPawnRow) || ((currentPawnCol - 1) != opponentPawnCol)) // Opponent pawn isn't leftwards
+		   ) {
+			int nextPawnPosition = currentPawnRow * 9 + (currentPawnCol - 1);
+			if (player) {
+				gameState.player1_pos = nextPawnPosition;
+			}
+			else {
+				gameState.player0_pos = nextPawnPosition;
+			}
+			legalNormalPawnMoves.push_back(gameState);
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	// check moving right
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (currentPawnCol != 8) { // inside board
+		if (!gameState.vertical_walls[currentPawnRow][currentPawnCol] && // No wall rightwards
+			((currentPawnRow != opponentPawnRow) || ((currentPawnCol + 1) != opponentPawnCol)) // Opponent pawn isn't rightwards
+		   ) {
+			int nextPawnPosition = currentPawnRow * 9 + (currentPawnCol + 1);
+			if (player) {
+				gameState.player1_pos = nextPawnPosition;
+			}
+			else {
+				gameState.player0_pos = nextPawnPosition;
+			}
+			legalNormalPawnMoves.push_back(gameState);
+		}
+	}
 
-vector<GameState> normalPawnMoves(GameState gamestate) {
-	// Implement the logic to calculate normal pawn moves up,right ,left, down
-	/*  if nextCell is outside the board:
-	  continue
-
-	  if wall exists between pawnPosition and nextCell:
-	  continue
-
-	  if nextCell == opponentPosition:
-	  continue*/
+	return legalNormalPawnMoves;
 }
 
-vector<GameState> jumpMoves(GameState gamestate) {
+vector<GameState> jumpMoves(GameState gameState, bool player) {
 	//moves = empty list
 	//
 	//if opponent is adjacent to myPawn:
@@ -49,7 +121,7 @@ vector<GameState> horizontalWallMoves(GameState gameState, bool player) {
 	// check all legal moves
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
-			if (gameState.horizontal_walls[i][j] ||		// cell is occupied
+			if (gameState.horizontal_walls[i][j] || // cell is occupied
 				gameState.horizontal_walls[i][j + 1] || // adjacent cell is occupied (horizontal wall occupy 2 cells)
 				(gameState.vertical_walls[i][j] && gameState.vertical_walls[i + 1][j]) // slot crosses vertical wall
 			) {
@@ -66,7 +138,8 @@ vector<GameState> horizontalWallMoves(GameState gameState, bool player) {
 			}
 			int shortest_path_player_0 = bfs(gameState, false);
 			int shortest_path_player_1 = bfs(gameState, true);
-			if (shortest_path_player_0 != -1 && shortest_path_player_1 != -1) {		// valid wall
+			if (shortest_path_player_0 != -1 && shortest_path_player_1 != -1) {
+				// valid wall
 				legalWallMoves.push_back(gameState);
 			}
 			// remove wall
@@ -100,10 +173,10 @@ vector<GameState> verticalWallMoves(GameState gameState, bool player) {
 	// check all legal moves
 	for (int j = 0; j < 8; j++) {
 		for (int i = 0; i < 8; i++) {
-			if (gameState.vertical_walls[i][j] ||		// cell is occupied
-				gameState.vertical_walls[i + 1][j] ||	// adjacent cell is occupied
-				(gameState.horizontal_walls[i][j] && gameState.horizontal_walls[i][j + 1])	// slot crosses horizontal wall
-				) {
+			if (gameState.vertical_walls[i][j] || // cell is occupied
+				gameState.vertical_walls[i + 1][j] || // adjacent cell is occupied
+				(gameState.horizontal_walls[i][j] && gameState.horizontal_walls[i][j + 1]) // slot crosses horizontal wall
+			) {
 				continue;
 			}
 			// temporary place wall and check if both players have a path
@@ -117,7 +190,8 @@ vector<GameState> verticalWallMoves(GameState gameState, bool player) {
 			}
 			int shortest_path_player_0 = bfs(gameState, false);
 			int shortest_path_player_1 = bfs(gameState, true);
-			if ((shortest_path_player_0 != -1) && (shortest_path_player_1 != -1)) {		// valid wall
+			if ((shortest_path_player_0 != -1) && (shortest_path_player_1 != -1)) {
+				// valid wall
 				legalWallMoves.push_back(gameState);
 			}
 			// remove wall
@@ -132,4 +206,46 @@ vector<GameState> verticalWallMoves(GameState gameState, bool player) {
 		}
 	}
 	return legalWallMoves;
+}
+
+vector<GameState> getAllLegalMoves(GameState gameState, bool player) {
+	vector<GameState> legalHorizontalWallMoves = horizontalWallMoves(gameState, player);
+	vector<GameState> legalVerticalWallMoves = verticalWallMoves(gameState, player);
+	vector<GameState> legalNormalPawnMoves = normalPawnMoves(gameState, player);
+	vector<GameState> legalJumpMoves = jumpMoves(gameState, player);
+
+	vector<GameState> allLegalMoves;
+	allLegalMoves.reserve(legalHorizontalWallMoves.size() + legalVerticalWallMoves.size() +
+		legalNormalPawnMoves.size() + legalJumpMoves.size());
+	allLegalMoves.insert(allLegalMoves.end(), legalHorizontalWallMoves.begin(), legalHorizontalWallMoves.end());
+	allLegalMoves.insert(allLegalMoves.end(), legalVerticalWallMoves.begin(), legalVerticalWallMoves.end());
+	allLegalMoves.insert(allLegalMoves.end(), legalNormalPawnMoves.begin(), legalNormalPawnMoves.end());
+	allLegalMoves.insert(allLegalMoves.end(), legalJumpMoves.begin(), legalJumpMoves.end());
+	return allLegalMoves;
+}
+
+vector<GameState> getAscendingHeuristicMoves(GameState gameState, bool player, int numberOfMoves) {
+	vector<GameState> allLegalMoves = getAllLegalMoves(gameState, player);
+	sort(allLegalMoves.begin(), allLegalMoves.end(),
+	     [](const GameState& a, const GameState& b) {
+		     return heuresticDistanceToGoalDifference(a) < heuresticDistanceToGoalDifference(b);
+	     }
+	);
+	if(allLegalMoves.size() > numberOfMoves) {
+		allLegalMoves.resize(numberOfMoves);
+	}
+	return allLegalMoves;
+}
+
+vector<GameState> getDescendingHeuristicMoves(GameState gameState, bool player, int numberOfMoves) {
+	vector<GameState> allLegalMoves = getAllLegalMoves(gameState, player);
+	sort(allLegalMoves.begin(), allLegalMoves.end(),
+	     [](const GameState& a, const GameState& b) {
+		     return heuresticDistanceToGoalDifference(a) > heuresticDistanceToGoalDifference(b);
+	     }
+	);
+	if(allLegalMoves.size() > numberOfMoves) {
+		allLegalMoves.resize(numberOfMoves);
+	}
+	return allLegalMoves;
 }
