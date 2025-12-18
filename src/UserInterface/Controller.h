1d5ddc76857dc44e91ea29c08b1dcf28be5e0d2a
RamezEmad02 <ramezemad20000@gmail.com>
2025-12-18 02:13:08 +0200

    ui basic win condition goal highlight

--- src/UserInterface/Controller.h
+++ src/UserInterface/Controller.h
@@ -4,6 +4,9 @@
 
 #ifndef QUORIDOR_GAME_CONTROLLER_H
 #define QUORIDOR_GAME_CONTROLLER_H
+//3iz vector of integers for positions of the pawn u take game state ys7bi
+//3iz vbox wa hbox
+//3iz gamestate mn AI
 
 
 
